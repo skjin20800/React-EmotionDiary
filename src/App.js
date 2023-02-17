6e711f9a435e317cb632cd62c55af470e25ace80
JinKyoungBin <skjin20800@naver.com>
2023-02-17 16:53:40 +0900

    web localStorage

--- src/App.js
+++ src/App.js
@@ -28,27 +28,32 @@ const reducer = (state, action) => {
     default:
       return state;
   }
+
+  localStorage.setItem('diary', JSON.stringify(newState));
   return newState;
 };
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  { id: 1, emotion: 1, content: '오늘의 일기 1번', date: 1676533523457 },
-  { id: 2, emotion: 2, content: '오늘의 일기 2번', date: 1676533523458 },
-  { id: 3, emotion: 3, content: '오늘의 일기 3번', date: 1676533523459 },
-  { id: 4, emotion: 4, content: '오늘의 일기 4번', date: 1676533523460 },
-  { id: 5, emotion: 5, content: '오늘의 일기 5번', date: 1676533523461 },
-];
-
 function App() {
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  const [data, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    const localData = localStorage.getItem('diary');
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort((a, b) => parseInt(b.id) - parseInt(a.id));
+      dataId.current = parseInt(diaryList[0].id) + 1;
+
+      dispatch({ type: 'INIT', data: diaryList });
+    }
+  }, []);
 
   const dataId = useRef(0);
   //CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({ type: 'CREATE', data: { id: dataId.current, date: new Date(date).getTime(), content, emotion } });
+    dataId.current++;
   };
   //REMOVE
   const onRemove = targetId => {
